d144fd7912b7c72425775c85c0a8f7041fa7c89e
KurtSalonga <kasalonga23-0391@cca.edu.ph>
2025-05-13 23:59:13 +0800

    Cash Drawer Page
    Cash Drawer Page Final Code

---
+++ cash-drawer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CASH DRAWER PAGE</title>
+    <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="cash-drawer.css">
+</head>
+<body>
+
+<div class="header">
+    <div class="menu">
+        <span class="menu-icon">&#9776;</span>
+        <span class="title-text">Cash Drawer</span>
+    </div>
+    <div class="logo">BREWeb</div>
+</div>
+
+<div class="container">
+    <div class="top-row">
+        <div class="card">
+            <h3>GCash Sales</h3>
+            <div class="amount">₱1,500</div>
+        </div>
+        <div class="card">
+            <h3>Cash Sales</h3>
+            <div class="amount">₱3,000</div>
+        </div>
+        <div class="card">
+            <h3>Grab Sales</h3>
+            <div class="amount">₱1,500</div>
+        </div>
+    </div>
+
+    <div class="bottom-row">
+        <div class="large-box">
+            <h3>Cash Added</h3>
+        </div>
+        <div class="large-box">
+            <h3>Cash Expenses</h3>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
