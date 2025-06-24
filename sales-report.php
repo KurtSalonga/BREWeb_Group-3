d47e7b3a0746a0fbe75509d5e7ff49bf58884426
KurtSalonga <kasalonga23-0391@cca.edu.ph>
2025-06-24 14:01:30 +0800

    UPDATE SALES-REPORT
    UPDATED SALES-REPORT PHP AND CSS

---
+++ sales-report.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>SALES REPORT</title>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&family=Irish+Grover&family=Joan&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="sales-report.css">
+</head>
+<body>
+
+  <div class="header">
+    <div class="menu">&#9776; <span class="menu-text">Sales Report</span></div>
+    <div class="logo">BREWeb</div>
+  </div>
+
+  <div class="title">Sales Report</div>
+
+  <div class="top-boxes">
+    <div class="box">
+      <h2>GCash Sales</h2>
+      <p>₱1,500</p>
+    </div>
+    <div class="box">
+      <h2>Cash Sales</h2>
+      <p>₱3,000</p>
+    </div>
+    <div class="box">
+      <h2>Cash Expenses</h2>
+    </div>
+    <div class="box">
+      <h2>Cash Added</h2>
+    </div>
+  </div>
+
+  <div class="bottom-boxes">
+    <div class="box">
+      <h2>NUMBER OF SALES</h2>
+    </div>
+    <div class="box">
+      <h2>REVENUE</h2>
+    </div>
+    <div class="box">
+      <h2>PROFIT</h2>
+    </div>
+    <div class="box">
+      <h2>LOSS</h2>
+    </div>
+  </div>
+
+</body>
+</html>
